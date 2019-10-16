cd238c184f8a95f1415446ba51bdff104033763a
Romain Delamare <romain@delama.re>
2019-10-16 11:03:32 +0200

    ✨  display user name when connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import {Icon, Layout} from 'antd';
 
 const {Header, Content} = Layout;
 
-const App = () => (
-  <Layout className="layout">
-    <Header>
-      <h1 style={{float: 'left', color: 'white', margin: 0}}>
-        <Icon type="deployment-unit" style={{paddingRight: '1rem'}}/>
-        Atoms
-      </h1>
-    </Header>
-    <Content style={{background: '#fff', padding: 24, minHeight: 280}}>
-      content
-    </Content>
-  </Layout>
-);
+const api = axios.create({
+  withCredentials: true,
+  baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+class App extends Component {
+  state = {
+    status: ''
+  };
+
+  componentDidMount() {
+    this.checkConnection();
+  }
+
+  checkConnection() {
+    api.get('me')
+      .then(res => this.setState({status: res.data.name}))
+      .catch(err => this.setState({status: 'not connected'}));
+  }
+
+  render() {
+    const {status} = this.state;
+    return (
+      <Layout className="layout">
+        <Header>
+          <h1 style={{float: 'left', color: 'white', margin: 0}}>
+            <Icon type="deployment-unit" style={{paddingRight: '1rem'}}/>
+            Atoms
+          </h1>
+          <div style={{float: 'right', color: 'white'}}>
+            {status}
+          </div>
+        </Header>
+        <Content style={{background: '#fff', padding: 24, minHeight: 280}}>
+          content
+        </Content>
+      </Layout>
+    );
+  }
+}
 
 export default App;
