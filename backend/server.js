824fa5d62a017e4dacb8fa418defcee452cad7c3
Romain Delamare <romain@delama.re>
2019-10-16 10:45:38 +0200

    🐛  fix cors request with credentials

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: (origin, callback) => callback(null, true), credentials: true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(session({
